cb66c5c1c4c985d4c2988294344a32d80c4bd8b1
Nalzurin <alexnormandi@ukr.net>
2024-02-08 01:46:52 +0200

    Implement client sending weather info packet Implement server decoding and sending information to the database

--- Lab2_Client/Program.cs
+++ Lab2_Client/Program.cs
@@ -2,34 +2,42 @@
 using System.Net;
 using System.Text;
 using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using MongoDB.Bson.Serialization.Serializers;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
 
 
 UdpClient udpClient = new UdpClient();
 HttpClient caller = new HttpClient();
 IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
-
+string apiKey = Environment.GetEnvironmentVariable("ApiKey");
 try
 {
     Console.Write("Enter city name: ");
     string city = Console.ReadLine().ToLower();
     while(true)
     {
-        string weatherInfo = await GetWeather(city);
+        WeatherPacket weatherInfo = await GetWeather(city);
         if(weatherInfo != null)
         {
-            
+            string jsonData = Serialize(weatherInfo);
+
+            // Convert the JSON string to a byte array
+            byte[] data = Encoding.UTF8.GetBytes(jsonData);
+            // Send the message to the server
+            udpClient.Send(data, data.Length, serverEndPoint);
+            Console.WriteLine("Data Sent");
+
+            // Receive the response from the server
+            byte[] receivedData = udpClient.Receive(ref serverEndPoint);
+            string response = Encoding.UTF8.GetString(receivedData);
+
+            Console.WriteLine($"Received response from server: {response}");
+            Thread.Sleep(60000);
+
         }
-        byte[] data = Encoding.UTF8.GetBytes(message);
 
-        // Send the message to the server
-        udpClient.Send(data, data.Length, serverEndPoint);
-
-        // Receive the response from the server
-        byte[] receivedData = udpClient.Receive(ref serverEndPoint);
-        string response = Encoding.UTF8.GetString(receivedData);
-
-        Console.WriteLine($"Received response from server: {response}");
-        Thread.Sleep(60000);
     }
 
     
@@ -43,9 +51,24 @@ finally
     udpClient.Close();
 }
 
-async Task<string> GetWeather(string city)
+async Task<WeatherPacket> GetWeather(string city)
 {
-    return await GetApiResponse($"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=");//Didn't put the api key for safety :P
+    string weatherInfoJsonRaw = await GetApiResponse($"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}");
+    JObject weather = JObject.Parse(weatherInfoJsonRaw);
+    JArray weatherArray = (JArray)weather["weather"];
+
+    JObject weatherInfo = (JObject)weatherArray.First();
+    string weatherType = (string)weatherInfo["main"];
+
+    JObject weatherDetails = (JObject)weather["main"];
+    string temperatureKelvin = (string)weatherDetails["temp"];
+
+    WeatherPacket weatherPacket = new WeatherPacket();
+    weatherPacket.Temperature = float.Round(float.Parse(temperatureKelvin) - 273.15f,2);
+    weatherPacket.Pressure = (float)weatherDetails["pressure"];
+    weatherPacket.Humidity = (float)weatherDetails["humidity"];
+    return weatherPacket;
+
 
 }
 
@@ -74,6 +97,10 @@ async Task<string> GetApiResponse(string apiUrl)
         Console.WriteLine($"Exception: {ex.Message}");
         return null;
     }
+}
+static string Serialize(object obj)
+{
+    return JsonSerializer.Serialize(obj);
 }
 
 [Serializable]
--- Lab2_Server/Program.cs
+++ Lab2_Server/Program.cs
@@ -1,8 +1,27 @@
 ﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
-
+using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using System.Diagnostics;
+using MongoDB.Bson.Serialization.Attributes;
 UdpClient udpServer = new UdpClient(12345);
+string connectionUri = Environment.GetEnvironmentVariable("MONGODB_URI");
+
+
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+// Set the ServerApi field of the settings object to set the version of the Stable API on the client
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+
+var collection = client.GetDatabase("weatherDataApp").GetCollection<BsonDocument>("weatherData");
+
+
 
 Console.WriteLine("UDP Server is listening on port 12345...");
 
@@ -10,10 +29,33 @@ while (true)
 {
     IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
     byte[] data = udpServer.Receive(ref clientEndPoint);
-    string message = Encoding.UTF8.GetString(data);
+    BsonDocument weatherDataBson = new BsonDocument();
+    WeatherPacket weatherData = Deserialize<WeatherPacket>(data);
+    weatherDataBson.Add("temperature", BsonValue.Create(weatherData.Temperature));
+    weatherDataBson.Add("pressure", BsonValue.Create(weatherData.Pressure));
+    weatherDataBson.Add("humidity", BsonValue.Create(weatherData.Humidity));
 
-    Console.WriteLine($"Received message from {clientEndPoint}: {message}");
 
-    // Echo the message back to the client
-    udpServer.Send(data, data.Length, clientEndPoint);
+    collection.InsertOne(weatherDataBson);
+    Console.WriteLine($"Received weather data from {clientEndPoint}:");
+    Console.WriteLine($"Temperature: {weatherData.Temperature}°C");
+    Console.WriteLine($"Humidity: {weatherData.Humidity}%");
+    Console.WriteLine($"Pressure: {weatherData.Pressure} hPa");
+
+    string response = "Data received successfully";
+
+    udpServer.Send(Encoding.UTF8.GetBytes(response), Encoding.UTF8.GetBytes(response).Length, clientEndPoint);
+}
+T Deserialize<T>(byte[] data)
+{
+    string jsonData = Encoding.UTF8.GetString(data);
+    return JsonSerializer.Deserialize<T>(jsonData);
+}
+
+[Serializable]
+public class WeatherPacket
+{
+    public float Temperature { get; set; }
+    public float Humidity { get; set; }
+    public float Pressure { get; set; }
 }
