6d7ea9e857ab9c7d05b383f16fa5c55414a55b9a
Nalzurin <alexnormandi@ukr.net>
2024-02-08 23:30:13 +0200

    Implement client and server for sending base64 images for lab4

---
+++ Lab4_Client/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+TcpClient tcpClient;
+
+List<int> packets = new List<int>();
+Random rand = new Random(67589);
+
+Console.WriteLine("Sending packet to remote server: ");
+while (true)
+{
+    try
+    {
+
+        int packetId = rand.Next();
+        if (!packets.Contains(packetId))
+        {
+            tcpClient = new TcpClient();
+            tcpClient.Connect("127.0.0.1", 12345);
+            packets.Add(packetId);
+            DateTime dateTimeStamp = DateTime.UtcNow;
+
+            string base64Image = CreateAndEncodeImage();
+
+            byte[] packetData = new byte[4 + 4 + base64Image.Length];
+
+            BitConverter.GetBytes(packetId).CopyTo(packetData, 0);
+            BitConverter.GetBytes((int)(dateTimeStamp.Subtract(new DateTime(1970, 1, 1))).TotalSeconds).CopyTo(packetData, 4);
+            Encoding.UTF8.GetBytes(base64Image).CopyTo(packetData, 8);
+
+            NetworkStream stream = tcpClient.GetStream();
+            stream.Write(packetData, 0, packetData.Length);
+            Console.WriteLine("Data Sent");
+            // Receive and display response from server
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Server Response: {0}", response);
+            stream.Close();
+            tcpClient.Close();
+            Thread.Sleep(10000);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+string CreateAndEncodeImage()
+{
+
+    
+    Bitmap image = new Bitmap(16, 16);
+    Random random = new Random();
+
+    for (int x = 0; x < 16; x++)
+    {
+        for (int y = 0; y < 16; y++)
+        {
+            Color randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+            image.SetPixel(x, y, randomColor);
+        }
+    }
+
+    
+    MemoryStream memoryStream = new MemoryStream();
+    image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+
+    
+    byte[] imageBytes = memoryStream.ToArray();
+    string base64Image = Convert.ToBase64String(imageBytes);
+
+    return base64Image;
+}
+
---
+++ Lab4_Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+int listenPort = 12345; 
+
+TcpListener tcpListener = new TcpListener(IPAddress.Any, listenPort);
+tcpListener.Start();
+
+
+Console.WriteLine("TCP Server is listening on port {0}...", listenPort);
+try
+{
+
+    while (true)
+    {
+        TcpClient tcpClient = tcpListener.AcceptTcpClient();
+        Console.WriteLine("Client connected.");
+
+        NetworkStream stream = tcpClient.GetStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        byte[] receivedData = new byte[bytesRead];
+        Array.Copy(buffer, receivedData, bytesRead);
+
+        ProcessTcpImagePacket(receivedData);
+
+        string responseMessage = "Image received successfully!";
+        byte[] responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+        stream.Write(responseBytes, 0, responseBytes.Length);
+        stream.Close();
+        tcpClient.Close();
+        Console.WriteLine("Client disconnected.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+void ProcessTcpImagePacket(byte[] packetData)
+{
+    uint packetId = BitConverter.ToUInt32(packetData, 0);
+    int unixTimestamp = BitConverter.ToInt32(packetData, 4);
+    DateTime dateTimeStamp = new DateTime(1970, 1, 1).AddSeconds(unixTimestamp);
+    string base64Image = Encoding.UTF8.GetString(packetData, 8, packetData.Length - 8);
+    SavePacketDetailsToJson(packetId, dateTimeStamp, base64Image);
+}
+
+
+
+void SavePacketDetailsToJson(uint packetId, DateTime dateTimeStamp, string base64Image)
+{
+    var packetDetails = new
+    {
+        PacketId = packetId,
+        DateTimeStamp = dateTimeStamp.ToString("yyyy-MM-dd HH:mm:ss"),
+        Base64Image = base64Image
+    };
+    string json = JsonConvert.SerializeObject(packetDetails);
+    string fileName = $"{packetId}.json";
+    File.WriteAllText(fileName, json);
+    Console.WriteLine($"Packet details saved to: {fileName}");
+}
